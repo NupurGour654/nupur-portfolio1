d3ede3c621e41646f7caa3dedd79d4f66656b461
NupurGour654 <155764854+NupurGour654@users.noreply.github.com>
2025-06-26 23:14:10 +0530

    Add  global javascript file.

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+
+const navLinks = document.querySelectorAll('.menu a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
